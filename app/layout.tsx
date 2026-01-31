6e549bd8662daa5062e24c6a4bfd16cf5b0ca34c
fridacisneros <soberanis.frida@gmail.com>
2026-01-31 13:45:44 -0700

    fix: light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="light" forcedTheme="light">
           <Navbar />
           <main className="min-h-screen">{children}</main>
         </ThemeProvider>
